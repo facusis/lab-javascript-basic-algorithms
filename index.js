7b094bac6b674b2ed9f725d1489defd4be2abf3d
Facu <65638351+facusis@users.noreply.github.com>
2025-02-19 17:09:35 +0100

    iteracion 1 terminada

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
 // Iteration 1: Names and Input
+const hacker1 = 'facundo'
+console.log('The driver´s name is ' +hacker1)
+
+const hacker2 = 'Eli'
+console.log('The navigator´s name is ' + hacker2)
+
 
 
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
