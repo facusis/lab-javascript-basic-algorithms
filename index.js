148f442bdfddbbe4ecc59401fda3d5d916e30a54
Facu <65638351+facusis@users.noreply.github.com>
2025-02-21 19:20:27 +0100

    iteracion 3 terminada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Iteration 1: Names and Input
-const hacker1 = 'Facundo'
+const hacker1 = 'acundo'
 console.log('The driver´s name is ' +hacker1)
 
 const hacker2 = 'Eli'
@@ -18,6 +18,43 @@ if(hacker1.length > hacker2.length) {
 }
 
 
-
 // Iteration 3: Loops
+let finalName = ''
+for (let i = 0; i < hacker1.length; i++) {
+  let name = hacker1[i].toUpperCase()
+  finalName += name + ''
+  
+}
 
+console.log(finalName);
+
+/* 3.2 */
+let finalNameReverse = ''
+for (let i = hacker2.length-1; i>= 0; i--) {
+  let name = hacker2[i];
+  finalNameReverse += name + ''
+}
+
+console.log(finalNameReverse)
+
+/* 3.3 */
+// const firstLeter = ''
+// for (let i = 0; i < hacker1.length; i++) {
+//   const element = array[i];
+  
+// }
+
+
+
+const firstLeter = hacker1.charAt(0).toLowerCase()
+const firstLeter2 = hacker2.charAt(0).toLowerCase()
+
+if(firstLeter < firstLeter2) {
+  console.log("The driver's name goes first")
+} else if (firstLeter > firstLeter2) {
+  console.log("Yo, the navigator goes first, definitely.")
+} else {
+  console.log("What?! You both have the same name?")
+}
+ 
+console.log(hacker1.localeCompare(hacker2))
